a14089cd3b199a019bcb06a1615a1ca02b4aa14a
ChrisCim <christenvieimbey040.com>
2026-01-14 23:29:12 +0100

    Update app (bug fix / new feature)

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ import 'api/client.dart';
 import 'api/app_update_check.dart';
 
 /// 🔥 VERSION APP (AFFICHAGE UNIQUEMENT)
-const String kAppVersion = "5.5.2";
+const String kAppVersion = "5.5.5";
 
 /// 🔔 Local notifications instance
 final FlutterLocalNotificationsPlugin localNotifications =
